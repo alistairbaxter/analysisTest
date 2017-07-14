62a83d25ad7925018a47750cc173eaf64d5f6d46
alistairbaxter <alistair.baxter@outlook.com>
2017-07-14 08:38:25 +0100

    Initial Commit

---
+++ testAnalyse/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  testAnalyse
+//
+//  Created by Alistair Baxter on 14/07/2017.
+//  Copyright © 2017 Alistair Baxter. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
