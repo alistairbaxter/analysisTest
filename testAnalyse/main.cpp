b55c005b913c961a4bdf9e389a94ea080ac624e2
alistairbaxter <alistair.baxter@outlook.com>
2017-07-18 18:06:34 +0100

    A set of 6 tests, now providing reasonable coverage, apart from filesystem or memory errors that are difficult to simulate.

--- testAnalyse/main.cpp
+++ testAnalyse/main.cpp
@@ -8,6 +8,7 @@
 
 #include <string>
 #include <iostream>
+#include <fstream>
 #include <assert.h>
 
 #include "analyse.hpp"
@@ -42,36 +43,75 @@ std::string errorDescription(int errNum)
 
 // Test Data
 enum{
-    NumTests = 2
+    NumTests = 6
 };
 
 std::string testInputFiles[NumTests] =
 {
     "test1.txt",
     "FileDoesNotExists.txt",
+    "test3generated.txt",
+    "test4generated.txt",
+    "test5.txt",
+    "test1.txt"
 };
 
 std::string testOutputFiles[NumTests] =
 {
-    "result1.txt"
-    "result2.txt"
+    "result1.txt",
+    "result2.txt",
+    "result3.txt",
+    "result4.txt",
+    "result5.txt",
+    ""
 };
 
 int expectedTestResults[NumTests] =
 {
     analysis::Error_NoError,
-    analysis::Error_InputFileInvalid
+    analysis::Error_InputFileInvalid,
+    analysis::Error_InvalidAnalyis,
+    analysis::Error_NoError,
+    analysis::Error_InvalidNumber,
+    analysis::Error_OutputFileInvalid
 };
 
 int64_t expectedOutputValues[NumTests][3] =
 {
     { 19, 135727, 7143 },
+    { 0, 0, 0 },
+    { 0, 0, 0 },
+    { 3, INT64_MAX / 2, INT64_MAX / 6 },
+    { 0, 0, 0 },
     { 0, 0, 0 }
-
 };
 
 int main(int argc, const char * argv[]) {
 
+    
+    int64_t veryLargeNumber = INT64_MAX / 2;
+    
+    // Generate a test file with very large numbers in
+    std::ofstream outputFile( testInputFiles[2] );
+    if (outputFile.is_open())
+    {
+        outputFile << veryLargeNumber << std::endl;
+        outputFile << veryLargeNumber << std::endl;
+        outputFile << veryLargeNumber << std::endl;
+    }
+    outputFile.close();
+    
+    // Generate a test file with very large, but valid numbers in
+    outputFile.open( testInputFiles[3] );
+    if (outputFile.is_open())
+    {
+        outputFile << veryLargeNumber << std::endl;
+        outputFile << -veryLargeNumber << std::endl;
+        outputFile << veryLargeNumber << std::endl;
+    }
+    outputFile.close();
+    
+    
     
     // Run through all tests
     for (int i=0; i< NumTests; i++)
@@ -86,11 +126,36 @@ int main(int argc, const char * argv[]) {
         if (result == analysis::Error_NoError)
         {
             // Test resulting values
+            
+            std::ifstream inputFile( testOutputFiles[i] );
+            
+            if (inputFile.is_open())
+            {
+                // Read a line
+                std::string line;
+                getline (inputFile,line);
+                int64_t countVal = atoll(line.c_str());
+                getline (inputFile,line);
+                int64_t sumVal = atoll(line.c_str());
+                getline (inputFile,line);
+                int64_t averageVal = atoll(line.c_str());
+
+                std::cout << "Expected output count: " << expectedOutputValues[i][0];
+                std::cout << " sum: " << expectedOutputValues[i][1];
+                std::cout << " average: " << expectedOutputValues[i][2] << std::endl;
+                
+                std::cout << "Actual output count: " << countVal;
+                std::cout << " sum: " << sumVal;
+                std::cout << " average: " << averageVal << std::endl;
+                
+                assert(countVal == expectedOutputValues[i][0]);
+                assert(sumVal == expectedOutputValues[i][1]);
+                assert(averageVal == expectedOutputValues[i][2]);
+            }
+            inputFile.close();
         }
-        
+    
     }
-    
-    
 
     
     return 0;
